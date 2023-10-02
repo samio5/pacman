1f8654400ace495fdc1904bdf1873cc0d69a09c5
Samio <voleocodes@gmail.com>
2023-10-02 23:48:10 +0300

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const grid = document.querySelector(".grid")
     const scoreDisplay = document.getElementById("score")
-    const width = 28 //* 28 x 28 = 784 squares.
+    const width = 28 // 28 x 28 = 784 squares.
+    let score = 0
 
-    //* layout of grid and what is in the squares.
+    // layout of grid and what is in the squares.
     const layout = [
         1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
         1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
@@ -49,7 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
             grid.appendChild(square)
             squares.push(square)
     
-          //add layout to the board
+            //add layout to the board
             if(layout[i] === 0) {
                 squares[i].classList.add("pac-dot")
             } else if (layout[i] === 1) {
@@ -64,21 +65,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     createBoard()
 
-    //* starting position of pacman
+    // starting position of pacman
     let pacmanCurrentIndex = 490
     squares[pacmanCurrentIndex].classList.add("pac-man")
 
-    //* move pacman
-    function movePacman(event) {
+    // move pacman
+    function movePacman(e) {
         squares[pacmanCurrentIndex].classList.remove("pac-man")
 
-        switch(event.keyCode) {
+        switch(e.keyCode) {
             /**
              * A = 65
              * W = 87
              * S = 83
              * D = 68
-             * 
              * 37	Arrow Left
              * 38	Arrow Up
              * 39	Arrow Right
@@ -88,28 +88,48 @@ document.addEventListener("DOMContentLoaded", () => {
                 if(pacmanCurrentIndex % width !== 0 && 
                     !squares[pacmanCurrentIndex -1].classList.contains("wall") &&
                     !squares[pacmanCurrentIndex -1].classList.contains("ghost-lair")) pacmanCurrentIndex -=1
-                break
+
+                    //check if pacman is in the left exit
+                    if(pacmanCurrentIndex -1 === 363) {
+                        pacmanCurrentIndex = 391
+                    }
+
+                break;
             case 38:
                 if(pacmanCurrentIndex - width >= 0 && 
                     !squares[pacmanCurrentIndex -width].classList.contains("wall") &&
                     !squares[pacmanCurrentIndex -width].classList.contains("ghost-lair")) pacmanCurrentIndex -=width
-                break
+                break;
             case 39:
                 if(pacmanCurrentIndex % width < width -1 && 
                     !squares[pacmanCurrentIndex +1].classList.contains("wall") && 
                     !squares[pacmanCurrentIndex +1].classList.contains("ghost-lair")) pacmanCurrentIndex +=1
-                break
+
+                    //check if pacman is in the right exit
+                    if(pacmanCurrentIndex +1 === 392) {
+                        pacmanCurrentIndex = 364
+                    }
+
+                break;
             case 40:
                 if(pacmanCurrentIndex + width < width * width && 
                     !squares[pacmanCurrentIndex +width].classList.contains("wall") && 
                     !squares[pacmanCurrentIndex +width].classList.contains("ghost-lair")) pacmanCurrentIndex +=width
-                break
+                break;
         }
     }
 
     squares[pacmanCurrentIndex].classList.add("pac-man")
 
-    //pacDotEaten()
+    // what happens when pacman eats a pac-dot
+    function pacDotEaten() {
+        if(squares[pacmanCurrentIndex].classList.contains("pac-dot")) {
+            score++
+        }
+
+    }
+
+
     //powerPelletEaten()
     //checkForGameOver()
     //checkForWin()
